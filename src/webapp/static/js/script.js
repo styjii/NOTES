71ec001241ce990bbc34abaf0794a46afa99a779
styjii <randrianarisonhtojo@gmail.com>
2025-04-22 09:54:11 +0300

    update js and html: adding a btn cancel when delete a table

--- src/webapp/static/js/script.js
+++ src/webapp/static/js/script.js
@@ -1,6 +1,8 @@
 const btnEditNote = document.querySelectorAll(".display-note .btn-edit")
-const btnCancel = document.querySelectorAll(".update-note .btn-cancel")
+const btnCancelUpdateNote = document.querySelectorAll(".update-note .btn-cancel")
 const btnDeleteNote = document.querySelectorAll(".display-note .btn-delete")
+const btnDeleteTable = document.querySelectorAll(".btn-option .btn-delete")
+const btnCancelDeleteTable = document.querySelectorAll(".btn-confirm .btn-cancel")
 
 btnEditNote.forEach(element => {
     element.addEventListener("click", e => {
@@ -10,7 +12,7 @@ btnEditNote.forEach(element => {
         displayNote.style.display = "none"
     })
 })
-btnCancel.forEach(element => {
+btnCancelUpdateNote.forEach(element => {
     element.addEventListener("click", e => {
         const updateNote = e.target.parentNode.parentNode.parentNode.parentNode
         const displayNote = updateNote.previousElementSibling
@@ -24,5 +26,23 @@ btnDeleteNote.forEach(element => {
         const formDeleteNote = displayNote.nextElementSibling.nextElementSibling
         
         formDeleteNote.submit()
+    })
+})
+btnDeleteTable.forEach(element => {
+    element.addEventListener("click", e => {
+        const btnOption = e.target.parentNode
+        const btnConfirm = btnOption.nextElementSibling
+
+        btnOption.style.display = "none"
+        btnConfirm.style.display = ""
+    })
+})
+btnCancelDeleteTable.forEach(element => {
+    element.addEventListener("click", e => {
+        const btnConfirm = e.target.parentNode
+        const btnOption = btnConfirm.previousElementSibling
+        
+        btnConfirm.style.display = "none"
+        btnOption.style.display = ""
     })
 })
